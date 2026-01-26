751e917cd02b11fd0671da81e3136796ad19350b
Rodolfo Souza <rodolfosouza595@gmail.com>
2026-01-25 21:27:20 -0300

    atualizaçao de atividade com teste de java script

---
+++ EXERCICIOS/ex026/mq003/script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menuBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+menuBtn.addEventListener("click",() =>{
+    sideMenu.classList.toggle("active");
+})
+
+
+console.log (alert("olha java script"))
